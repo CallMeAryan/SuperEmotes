b46932e298dcf4c5d4dccdc2fb043b1ebc8ec1b7
BuildTools <unconfigured@null.spigotmc.org>
2022-06-06 10:53:44 +0530

    -Fixed some miner issues

--- src/main/java/liven/emotesplugin/Commands/EmotesCommand.java
+++ src/main/java/liven/emotesplugin/Commands/EmotesCommand.java
@@ -1,6 +1,5 @@
 package liven.emotesplugin.Commands;
 
-import liven.emotesplugin.EmotesPlugin;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -9,10 +8,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import javax.swing.plaf.IconUIResource;
-import java.util.UUID;
-
-import static liven.emotesplugin.EmotesPlugin.*;
+import static liven.emotesplugin.EmotesPlugin.EnabledEmojis;
+import static liven.emotesplugin.EmotesPlugin.dic;
 
 public class EmotesCommand implements CommandExecutor {
 
@@ -20,63 +17,64 @@ public class EmotesCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
-
+        ItemStack air = new ItemStack(Material.AIR);
         if (sender instanceof Player) {
 
 
             if (args.length == 1 && args[0].equalsIgnoreCase("help")) {
 
                 player.sendMessage(ChatColor.GREEN + "Choose between 1-8! Eg. /emotes 5");
-            } else if (args.length == 1 && args[0].equalsIgnoreCase("1")) {
+            }
+            if (player.hasPermission("emote.get." + args[0])) if (args.length == 1 && args[0].equalsIgnoreCase("1")) {
 
 
                 dic.put(player.getDisplayName(), 1);
                 EnabledEmojis.add(player.getUniqueId());
 
 
-            }else if (args.length == 1 && args[0].equalsIgnoreCase("2")) {
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("2")) {
 
 
                 dic.put(player.getDisplayName(), 2);
                 EnabledEmojis.add(player.getUniqueId());
 
 
-        }else if (args.length == 1 && args[0].equalsIgnoreCase("3")) {
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("3")) {
 
 
                 dic.put(player.getDisplayName(), 3);
                 EnabledEmojis.add(player.getUniqueId());
 
 
-            }else if (args.length == 1 && args[0].equalsIgnoreCase("4")) {
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("4")) {
 
 
                 dic.put(player.getDisplayName(), 4);
                 EnabledEmojis.add(player.getUniqueId());
 
 
-            }else if (args.length == 1 && args[0].equalsIgnoreCase("5")) {
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("5")) {
 
 
                 dic.put(player.getDisplayName(), 5);
                 EnabledEmojis.add(player.getUniqueId());
 
 
-            }else if (args.length == 1 && args[0].equalsIgnoreCase("6")) {
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("6")) {
 
 
                 dic.put(player.getDisplayName(), 6);
                 EnabledEmojis.add(player.getUniqueId());
 
 
-            }else if (args.length == 1 && args[0].equalsIgnoreCase("7")) {
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("7")) {
 
 
                 dic.put(player.getDisplayName(), 7);
                 EnabledEmojis.add(player.getUniqueId());
 
 
-            }else if (args.length == 1 && args[0].equalsIgnoreCase("8")) {
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("8")) {
 
 
                 dic.put(player.getDisplayName(), 8);
@@ -84,15 +82,16 @@ public class EmotesCommand implements CommandExecutor {
 
 
             } else {
-
-                dic.put(player.getDisplayName(), 0);
-                ItemStack air = new ItemStack(Material.AIR);
+                player.sendMessage(ChatColor.RED + "Emote Disabled");
+                dic.remove(player.getDisplayName());
+                EnabledEmojis.remove(player.getUniqueId());
                 player.getInventory().setHelmet(air);
 
             }
-    }
+        }
         return true;
-    } }
+    }
+}
 
 
 
